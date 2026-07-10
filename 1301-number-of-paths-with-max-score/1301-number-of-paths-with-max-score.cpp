bf701081c212835b1b4da858306d8503b77a6b40
Sumant Jha <72351139+sumant07official@users.noreply.github.com>
2026-07-10 16:36:59 +0530

    Time: 18 ms (22.37%), Space: 18 MB (14.56%) - LeetHub

---
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> pathsWithMaxScore(vector<string>& board) {
+        int n = board.size();
+        const int MOD = 1e9 + 7;
+
+        vector<vector<int>> score(n, vector<int>(n, -1));
+        vector<vector<int>> ways(n, vector<int>(n, 0));
+
+        score[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (board[i][j] == 'X') continue;
+                if (i == n - 1 && j == n - 1) continue;
+
+                int best = -1;
+                long long cnt = 0;
+
+                vector<pair<int,int>> dir = {{1,0},{0,1},{1,1}};
+
+                for (auto &[dx,dy] : dir) {
+                    int ni = i + dx;
+                    int nj = j + dy;
+
+                    if (ni >= n || nj >= n) continue;
+                    if (score[ni][nj] == -1) continue;
+
+                    if (score[ni][nj] > best) {
+                        best = score[ni][nj];
+                        cnt = ways[ni][nj];
+                    } else if (score[ni][nj] == best) {
+                        cnt = (cnt + ways[ni][nj]) % MOD;
+                    }
+                }
+
+                if (best == -1) continue;
+
+                int val = 0;
+                if (board[i][j] >= '1' && board[i][j] <= '9')
+                    val = board[i][j] - '0';
+
+                score[i][j] = best + val;
+                ways[i][j] = cnt % MOD;
+            }
+        }
+
+        if (ways[0][0] == 0)
+            return {0, 0};
+
+        return {score[0][0], ways[0][0]};
+    }
+};
