fc6b7e720f2a67c2240257bfb1a7a240ce19f2c5
Sumant Jha <72351139+sumant07official@users.noreply.github.com>
2026-09-10 11:11:49 +0530

    Time: 5 ms (72.11%), Space: 15.9 MB (29.76%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int ans = 0;
+
+    pair<int,int> dfs(TreeNode* root) {
+
+        if (!root)
+            return {0, 0};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        int sum = left.first + right.first + root->val;
+        int cnt = left.second + right.second + 1;
+
+        if (sum / cnt == root->val)
+            ans++;
+
+        return {sum, cnt};
+    }
+
+    int averageOfSubtree(TreeNode* root) {
+
+        dfs(root);
+
+        return ans;
+    }
+};
